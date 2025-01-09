69972c6c93de578d174c112e49ce38146c9556c7
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-01-09 16:33:23 +0200

    Login Page Done

---
+++ Phase2/src/App.jsx
@@ -0,0 +1,14 @@
+import LoginForm from './components/LoginForm'
+
+function App() {
+  
+  return (
+    <>
+      <div className="bg-primary min-h-screen flex items-center justify-center">
+        <LoginForm />
+      </div>
+    </>
+  )
+}
+
+export default App
